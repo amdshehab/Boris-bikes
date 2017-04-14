bf8eabb9414a5b6dbb0a283586a8e7058ad4f66d
Adam Snow <adamjohnsnow@icloud.com>
2017-04-14 15:46:30 +0100

    step 20 - half way - van collects all bikes

--- spec/borisbike_spec.rb
+++ spec/borisbike_spec.rb
@@ -1,42 +1,42 @@
 require 'docking_station'
 
 describe DockingStation do
-let(:bike) {double(:bike)}
+  let(:bike) {double(:bike)}
 
   it " creates an instance of DockingStation" do
     expect(DockingStation.new).to be_an_instance_of(DockingStation)
   end
 
- it {is_expected.to respond_to(:release_bike)}
+  it {is_expected.to respond_to(:release_bike)}
 
-#  it 'expects DockingStation to get a bike' do
-#    subject.dock(double(:bike).new)
-#    expect(subject.release_bike).to be_an_instance_of(double(:bike))
-# end
+  #  it 'expects DockingStation to get a bike' do
+  #    subject.dock(double(:bike).new)
+  #    expect(subject.release_bike).to be_an_instance_of(double(:bike))
+  # end
 
-# it 'expects bike to be working' do
-#   subject.dock(double(:bike).new)
-#   expect(subject.release_bike.working?).to eq true
-#  end
+  # it 'expects bike to be working' do
+  #   subject.dock(double(:bike).new)
+  #   expect(subject.release_bike.working?).to eq true
+  #  end
 
- it 'expects docking_station to be initialized with an argument' do
-   docking_station = DockingStation.new
- expect(docking_station).to respond_to(:dock).with(1).argument
- end
-
- it 'expects docking_station to respond to the dock method' do
-   docking_station = DockingStation.new
-   expect(docking_station).to respond_to(:dock)
- end
-
- it 'should raise an error when @bike = nil when releasing' do
-   if @capacity == nil
-     expect{subject.release_bike}.to raise_error("No working bikes available")
+  it 'expects docking_station to be initialized with an argument' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:dock).with(1).argument
   end
-end
+  
+  it 'expects docking_station to respond to the dock method' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:dock)
+  end
+
+  it 'should raise an error when @bike = nil when releasing' do
+    if @capacity == nil
+      expect{subject.release_bike}.to raise_error("No working bikes available")
+    end
+  end
 
   it 'should raise an error when docked bikes = 20 and docking' do
-     DockingStation::DEFAULT_CAPACITY.times {subject.dock(double(:bike))}
+    DockingStation::DEFAULT_CAPACITY.times {subject.dock(double(:bike))}
     expect{  subject.dock(double(:bike))}.to raise_error("full up")
   end
 
@@ -53,12 +53,12 @@ end
   it "identify the first available working bike" do
     bike = double(:bike)
     allow(bike).to receive(:working).and_return(false)
-      subject.dock(bike)
-      bike2 = double(:bike)
-      allow(bike2).to receive(:working).and_return(true)
-      subject.dock(bike2)
-      expect(subject.find_bike).to eq 1
+    subject.dock(bike)
+    bike2 = double(:bike)
+    allow(bike2).to receive(:working).and_return(true)
+    subject.dock(bike2)
+    expect(subject.find_bike).to eq 1
   end
 
 
- end
+end
